7a7d1aada44d22e436772e241633b66ec420979a
Joao Albuquerque <joaopealbuquerque1@gmail.com>
2022-01-02 20:58:13 -0300

    Adicionando componente de título

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -2,8 +2,8 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-    background-color:#17181F;
-    color: #797A81;
+    background: rgb(2,0,36);
+    background: linear-gradient(180deg, rgba(2,0,36,1) 48%, rgba(1,11,87,0.9970815373563219) 100%);
     min-height: 100vh;
 `
 
@@ -11,4 +11,13 @@ export const AreaTask = styled.div`
     margin: auto;
     max-width: 980px;
     padding: 10px;
+`
+
+export const Header = styled.h1`
+    margin:0;
+    padding:0;
+    color:#FFF;
+    text-align:center;
+    border-bottom:1px solid white;
+    padding-bottom:20px;
 `
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { useState } from 'react';
+
 import * as Components from './App.styles'
 
 const app = () => {
-  return(
+
+  const [list,setList] = useState([]);
+
+  return (
     <Components.Container>
       <Components.AreaTask>
-        ...
+        <Components.Header> Lista de Tarefas</Components.Header>
       </Components.AreaTask>
     </Components.Container>
   )
