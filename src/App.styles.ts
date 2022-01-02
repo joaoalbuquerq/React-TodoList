b05136f85dd0e0e97a0276d3a1aa4b2cbfff07da
Joao Albuquerque <joaopealbuquerque1@gmail.com>
2022-01-02 20:45:23 -0300

    Adicionando componente que centraliza ao meio todo o conteúdo

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -6,3 +6,9 @@ export const Container = styled.div`
     color: #797A81;
     min-height: 100vh;
 `
+
+export const AreaTask = styled.div`
+    margin: auto;
+    max-width: 980px;
+    padding: 10px;
+`
--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import * as Components from './App.styles'
 const app = () => {
   return(
     <Components.Container>
-      ...
+      <Components.AreaTask>
+        ...
+      </Components.AreaTask>
     </Components.Container>
   )
 }
