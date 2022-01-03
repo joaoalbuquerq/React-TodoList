839514d4719bbbd4d6d100633b25e94ed6f711fd
Joao Albuquerque <joaopealbuquerque1@gmail.com>
2022-01-02 21:39:39 -0300

    Adicionando itens a lista

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
 import { useState } from 'react';
-
+import {Item} from './types/Item'
 import * as Components from './App.styles'
 
-const app = () => {
+const App = () => {
 
-  const [list,setList] = useState([]);
+  const [list,setList] = useState<Item[]>([
+    {id:1,name:'Comprar pão',done:false},
+    {id:2,name:'Estudar React',done:false},
+  ]);
 
   return (
     <Components.Container>
       <Components.AreaTask>
         <Components.Header> Lista de Tarefas</Components.Header>
+
+
+        {list.map((item,index) =>(
+          <div>{item.name}</div>
+        ))}
       </Components.AreaTask>
     </Components.Container>
-  )
+  );
 }
 
-export default app;
+export default App;
