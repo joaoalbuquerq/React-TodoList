4bc399304c552cc1f4c053424f171e47df004770
Joao Albuquerque <joaopealbuquerque1@gmail.com>
2022-01-03 20:37:22 -0300

    Adicionando estilização do imput de new task

--- src/components/AddArea/index.tsx
+++ src/components/AddArea/index.tsx
@@ -4,7 +4,8 @@ export const AddArea = () => {
 
     return (
         <C.ContainerArea>
-            ...
+            <div className='image'>➕</div>
+            <input type="text" placeholder='Adicionar uma tarefa'/>
         </C.ContainerArea>
     )
 }
